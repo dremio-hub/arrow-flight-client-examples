8824a56f3a36c64137d45b1f895dd823e849bff3
TiffanyLam <tiffanyl@bitquilltech.com>
2020-10-21 13:12:34 -0700

    DX-25892: Arrow Flight client example in Python
    - Modified example.py
    - Modfied README.md

--- python/README.md
+++ python/README.md
@@ -1,11 +1,25 @@
 # Python Arrow Flight Client Exmaple
-The Arrow Flight endpoint in Dremio is exposed on port `32010`.
+This lightweight Python client connects to the Dremio Arrow Flight service endpoint. This client requires the username and password for authentication. The hostname and port used is default to `localhost` and `32010`, these settings can be changed by providing the hostname and port as arguments when running the client. Moreover, the tls option can be provided to establish an encrypted connection. Please note that trusted certificates must be provided when the tls option is enabled. This client can query any datasets in Dremio that are accessible by the Dremio user provided.
 
 ## Instructions on using this Python client
-- Install and setup Python3 
-- Install pyarrow
-  - `pip install pyarrow`
-- Customize `example.py`
-  - Replace `username`, `password` with the corresponding credentials of your Dremio user
-  - Replace `sql` with the query you want to run in Dremio
-- From a command line or terminal window, execute `example.py` with `python3 example.py`
+- Install and setup Python3 as `pyarrow` requires Python3
+- Install `pyarrow` package using pip
+  - `pip3 install pyarrow`
+- Install `pandas` package using pip
+  - `pip3 install pandas`
+- Run the Python client:
+  - `python3 example.py -host '<DREMIO_HOST>' -user '<DREMIO_USERNAME>' -pass '<DREMIO_PASSWORD>'`
+
+```
+usage: example.py [-h] [-host HOSTNAME] [-port FLIGHTPORT] -user USERNAME -pass PASSWORD [-query SQLQUERY] [-tls] [-certs TRUSTEDCERTIFICATES]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -host HOSTNAME, --hostname HOSTNAME Dremio co-ordinator hostname
+  -port FLIGHTPORT, --flightPort FLIGHTPORT Dremio flight server port
+  -user USERNAME, --username USERNAME Dremio username
+  -pass PASSWORD, --password PASSWORD Dremio password
+  -query SQLQUERY, --sqlquery SQLQUERY SQL query to test
+  -tls, --tls Enable encrypted connection
+  -certs TRUSTEDCERTIFICATES, --trustedCertificates TRUSTEDCERTIFICATES Path to trusted certificates for encrypted connection
+```
--- python/example.py
+++ python/example.py
@@ -1,35 +1,90 @@
 from pyarrow import flight
-import pyarrow as pa
+import argparse
+import traceback
 
-class HttpDremioClientAuthHandler(flight.ClientAuthHandler):
+# DremioServerClientAuthHandler extends flight.ClientAuthHandler
+class DremioBasicServerAuthHandler(flight.ClientAuthHandler):
+  # Constructor
+  def __init__(self, username, password):
+    self.username = username
+    self.password = password
+    super(flight.ClientAuthHandler, self).__init__()
 
-    def __init__(self, username, password):
-        super(flight.ClientAuthHandler, self).__init__()
-        self.basic_auth = flight.BasicAuth(username, password)
-        self.token = None
+  def authenticate(self, outgoing, incoming):
+    basicAuth = flight.BasicAuth(self.username, self.password)
+    outgoing.write(basicAuth.serialize())
+    self.token = incoming.read()
 
-    def authenticate(self, outgoing, incoming):
-        auth = self.basic_auth.serialize()
-        outgoing.write(auth)
-        self.token = incoming.read()
+  def get_token(self):
+    return self.token
 
-    def get_token(self):
-        return self.token
+def parseArguments():
+  """
+  Parses the command-line arguments supplied to the script.
+  """
+  # Process command line arguments.
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-host', '--hostname', type=str, help='Dremio co-ordinator hostname', default='localhost')
+  parser.add_argument('-port', '--flightPort', type=str, help='Dremio flight server port', default='32010')
+  parser.add_argument('-user', '--username', type=str, help='Dremio username', required=True)
+  parser.add_argument('-pass', '--password', type=str, help='Dremio password', required=True)
+  parser.add_argument('-query', '--sqlquery', type=str, help='SQL query to test', required=False)
+  parser.add_argument('-tls', '--tls', dest='tls', help='Enable encrypted connection', required=False, default=False, action='store_true')
+  parser.add_argument('-certs', '--trustedCertificates', type=str, help='Path to trusted certificates for encrypted connection', required=False)
+  return parser.parse_args()
 
-username = '<USERNAME>'
-password = '<PASSWORD>'
-sql = '''<SQL_QUERY>'''
+def connectToFlightServer(hostname, flightPort, username, password, sqlquery, tls, certs):
+  """
+   Connects to the Dremio Server and executes the specified Flight API calls.
 
-client = flight.FlightClient('grpc+tcp://<DREMIO_COORDINATOR_HOST>:32010')
-client.authenticate(HttpDremioClientAuthHandler(username, password)) 
-info = client.get_flight_info(flight.FlightDescriptor.for_command(sql))
-reader = client.do_get(info.endpoints[0].ticket)
-batches = []
-while True:
-    try:
-        batch, metadata = reader.read_chunk()
-        batches.append(batch)
-    except StopIteration:
-        break
-data = pa.Table.from_batches(batches)
-df = data.to_pandas()
+  Args:
+      hostname : Hostname to connect to.
+      port : Flight server port.
+      username : Dremio username to use for authentication.
+      password : Dremio password to use for authentication.
+      sqlquery : SQL query to test.
+      tls : Enable encrypted connection.
+      certs : Path to trusted certificates for a TLS connection.
+  """
+
+  try:
+    scheme = "grpc+tcp"
+    connection_args = {}
+    if tls:
+      print('[INFO] Enabling TLS connection')
+      scheme = "grpc+tls"
+      if certs:
+        print('[INFO] Trusted certificates provided')
+        with open(certs, "rb") as root_certs:
+                connection_args["tls_root_certs"] = root_certs.read()
+      else:
+        print('[Error] Trusted certificates must be provided to establish a TLS connection')
+        exit()
+
+    client = flight.FlightClient(f"{scheme}://{hostname}:{flightPort}",**connection_args)
+    client.authenticate(DremioBasicServerAuthHandler(username, password))
+    print('[INFO] Authentication was successful')
+
+    if sqlquery:
+      flight_desc = flight.FlightDescriptor.for_command(sqlquery)
+      schema = client.get_schema(flight_desc)
+      print('[INFO] GetSchema was successful')
+      print('[INFO] Schema: ', schema)
+      flight_info = client.get_flight_info(flight.FlightDescriptor.for_command(sqlquery))
+      print('[INFO] GetFlightInfo was successful')
+      print('[INFO] Ticket: ', flight_info.endpoints[0].ticket)
+      reader = client.do_get(flight_info.endpoints[0].ticket)
+      print('[INFO] Reading query results from Dremio')
+      print(reader.read_pandas())
+      print('[INFO] GetStream was successful')
+
+  except Exception as e:
+    # Print the exception along with the stack trace.
+    print(f'[ERROR] Exception: {repr(e)}')
+    traceback.print_exc()
+
+if __name__ == "__main__":
+  # Parse the command line arguments.
+  args = parseArguments()
+  # Connecto to Dremio Arrow Flight server.
+  connectToFlightServer(args.hostname, args.flightPort, args.username, args.password, args.sqlquery, args.tls, args.trustedCertificates)
