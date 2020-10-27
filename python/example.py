5ed907e6486fab6ced19505d860be437069fcce5
Tiffany Lam <tiffanylamhl@gmail.com>
2020-10-26 21:47:48 -0700

    Python workflow with Dremio service, flak8 and pytest (#3)
    * Add Python build workflow and correct example.py styling
    * Change build status badge to reference dremio-hub and add a line at the end of workflow yaml
    * Add Dremio service
    
    - Corrected styling of example.py
    - Added build status badge to python/README.md
    - Added Github actions build and lint workflow for example.py
    
    
    
    * Add python workflow with Dremio service
    
    - Added workflow to start up Dremio service.
    - In the same workflow, added steps to lint example.py.
    - Also added e2e tests for example.py.
    - example.py is formatted with standard indentation and block comments.
    - Added TLS server name override to the Flight Client sample application.
    
    * Modify unit tests names
    
    * Modify status badge link to work with dremio-hub
    
    * Add scheduled workflows for every Monday at 9am PST
    
    * Fix incorrect comment
    
    * Address review comments
    
    * Remove unused import in test.py
    
    * Modify workflow to run on all branches
    
    Co-authored-by: TiffanyLam <tiffanyl@bitquilltech.com>

--- python/example.py
+++ python/example.py
@@ -1,118 +1,131 @@
 """
   Copyright (C) 2017-2018 Dremio Corporation
- 
+
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
- 
+
       http://www.apache.org/licenses/LICENSE-2.0
- 
+
   Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   See the License for the specific language governing permissions and
   limitations under the License.
 """
+import argparse
+import sys
 
 from pyarrow import flight
-import argparse
-import traceback
+
 
 class DremioBasicServerAuthHandler(flight.ClientAuthHandler):
-  def __init__(self, username, password):
-    self.username = username
-    self.password = password
-    super(flight.ClientAuthHandler, self).__init__()
-  
-  def authenticate(self, outgoing, incoming):
-    # Authenticate with Dremio user credentials.
-    basicAuth = flight.BasicAuth(self.username, self.password)
-    outgoing.write(basicAuth.serialize())
-    self.token = incoming.read()
+    """
+    ClientAuthHandler for connections to Dremio server endpoint.
+    """
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        super(flight.ClientAuthHandler, self).__init__()
 
-  def get_token(self):
-    return self.token
+    def authenticate(self, outgoing, incoming):
+        """
+        Authenticate with Dremio user credentials.
+        """
+        basic_auth = flight.BasicAuth(self.username, self.password)
+        outgoing.write(basic_auth.serialize())
+        self.token = incoming.read()
 
-def parseArguments():
-  """
-  Parses the command-line arguments supplied to the script.
-  """
-  # Process command line arguments.
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-host', '--hostname', type=str, help='Dremio co-ordinator hostname', default='localhost')
-  parser.add_argument('-port', '--flightPort', type=str, help='Dremio flight server port', default='32010')
-  parser.add_argument('-user', '--username', type=str, help='Dremio username', required=True)
-  parser.add_argument('-pass', '--password', type=str, help='Dremio password', required=True)
-  parser.add_argument('-query', '--sqlquery', type=str, help='SQL query to test', required=False)
-  parser.add_argument('-tls', '--tls', dest='tls', help='Enable encrypted connection', required=False, default=False, action='store_true')
-  parser.add_argument('-certs', '--trustedCertificates', type=str, help='Path to trusted certificates for encrypted connection', required=False)
-  return parser.parse_args()
+    def get_token(self):
+        """
+        Get the token from this AuthHandler.
+        """
+        return self.token
 
-def connectToDremioFlightServerEndpoint(hostname, flightPort, username, password, sqlquery, tls, certs):
-  """
-   Connects to the Dremio Server and executes the specified Flight API calls.
 
-  Args:
-      hostname : Hostname to connect to.
-      port : Flight server port.
-      username : Dremio username to use for authentication.
-      password : Dremio password to use for authentication.
-      sqlquery : SQL query to test.
-      tls : Enable encrypted connection.
-      certs : Path to trusted certificates for a TLS connection.
-  """
+def parse_arguments():
+    """
+    Parses the command-line arguments supplied to the script.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-host', '--hostname', type=str, help='Dremio co-ordinator hostname',
+      default='localhost')
+    parser.add_argument('-port', '--flightport', type=str, help='Dremio flight server port',
+      default='32010')
+    parser.add_argument('-user', '--username', type=str, help='Dremio username',
+      required=True)
+    parser.add_argument('-pass', '--password', type=str, help='Dremio password',
+      required=True)
+    parser.add_argument('-query', '--sqlquery', type=str, help='SQL query to test',
+      required=False)
+    parser.add_argument('-tls', '--tls', dest='tls', help='Enable encrypted connection',
+      required=False, default=False, action='store_true')
+    parser.add_argument('-certs', '--trustedCertificates', type=str,
+      help='Path to trusted certificates for encrypted connection', required=False)
+    return parser.parse_args()
 
-  try:
-    # Default to use an unencrypted TCP connection.
-    scheme = "grpc+tcp"
-    connection_args = {}
-    
-    if tls:
-      # Connect to the server endpoint with an encrypted TLS connection.
-      print('[INFO] Enabling TLS connection')
-      scheme = "grpc+tls"
-      if certs:
-        print('[INFO] Trusted certificates provided')
-        # TLS certificates are provided in a list of connection arguments.
-        with open(certs, "rb") as root_certs:
-                connection_args["tls_root_certs"] = root_certs.read()
-      else:
-        print('[ERROR] Trusted certificates must be provided to establish a TLS connection')
-        exit()
 
-    client = flight.FlightClient(f"{scheme}://{hostname}:{flightPort}",**connection_args)
-    
-    # Authenticate with the server endpoint.
-    client.authenticate(DremioBasicServerAuthHandler(username, password))
-    print('[INFO] Authentication was successful')
+def connect_to_dremio_flight_server_endpoint(hostname, flightport, username, password, sqlquery,
+  tls, certs):
+    """
+    Connects to Dremio Flight server endpoint with the provided credentials.
+    It also runs the query and retrieves the result set.
+    """
 
-    if sqlquery:
-      # Construct FlightDescriptor for the query result set.
-      flight_desc = flight.FlightDescriptor.for_command(sqlquery)
-      print('[INFO] Query: ', sqlquery)
-      
-      # Retrieve the schema of the result set.
-      schema = client.get_schema(flight_desc)
-      print('[INFO] GetSchema was successful')
-      print('[INFO] Schema: ', schema)
-      
-      # Get the FlightInfo message to retrieve the Ticket corresponding to the query result set.
-      flight_info = client.get_flight_info(flight.FlightDescriptor.for_command(sqlquery))
-      print('[INFO] GetFlightInfo was successful')
-      print('[INFO] Ticket: ', flight_info.endpoints[0].ticket)
-      
-      # Retrieve the result set as a stream of Arrow record batches.
-      reader = client.do_get(flight_info.endpoints[0].ticket)
-      print('[INFO] Reading query results from Dremio')
-      print(reader.read_pandas())
+    try:
+        # Default to use an unencrypted TCP connection.
+        scheme = "grpc+tcp"
+        connection_args = {}
 
-  except Exception as e:
-    print(f'[ERROR] Exception: {repr(e)}')
-    # Uncomment the line below to enable stack trace printing.
-    # traceback.print_exc()
+        if tls:
+            # Connect to the server endpoint with an encrypted TLS connection.
+            print('[INFO] Enabling TLS connection')
+            scheme = "grpc+tls"
+            if certs:
+                print('[INFO] Trusted certificates provided')
+                # TLS certificates are provided in a list of connection arguments.
+                with open(certs, "rb") as root_certs:
+                    connection_args["tls_root_certs"] = root_certs.read()
+            else:
+                print('[ERROR] Trusted certificates must be provided to establish a TLS connection')
+                sys.exit()
+
+        client = flight.FlightClient("{}://{}:{}".format(scheme, hostname, flightport),
+          **connection_args)
+
+        # Authenticate with the server endpoint.
+        client.authenticate(DremioBasicServerAuthHandler(username, password))
+        print('[INFO] Authentication was successful')
+
+        if sqlquery:
+            # Construct FlightDescriptor for the query result set.
+            flight_desc = flight.FlightDescriptor.for_command(sqlquery)
+            print('[INFO] Query: ', sqlquery)
+
+            # Retrieve the schema of the result set.
+            schema = client.get_schema(flight_desc)
+            print('[INFO] GetSchema was successful')
+            print('[INFO] Schema: ', schema)
+
+            # Get the FlightInfo message to retrieve the Ticket corresponding
+            # to the query result set.
+            flight_info = client.get_flight_info(flight.FlightDescriptor.for_command(sqlquery))
+            print('[INFO] GetFlightInfo was successful')
+            print('[INFO] Ticket: ', flight_info.endpoints[0].ticket)
+
+            # Retrieve the result set as a stream of Arrow record batches.
+            reader = client.do_get(flight_info.endpoints[0].ticket)
+            print('[INFO] Reading query results from Dremio')
+            print(reader.read_pandas())
+
+    except Exception as exception:
+        print("[ERROR] Exception: {}".format(repr(exception)))
+        raise
+
 
 if __name__ == "__main__":
-  # Parse the command line arguments.
-  args = parseArguments()
-  # Connect to Dremio Arrow Flight server endpoint.
-  connectToDremioFlightServerEndpoint(args.hostname, args.flightPort, args.username, args.password, args.sqlquery, args.tls, args.trustedCertificates)
+    # Parse the command line arguments.
+    args = parse_arguments()
+    # Connect to Dremio Arrow Flight server endpoint.
+    connect_to_dremio_flight_server_endpoint(args.hostname, args.flightport, args.username,
+      args.password, args.sqlquery, args.tls, args.trustedCertificates)
---
+++ python/test.py
@@ -0,0 +1,67 @@
+"""
+  Copyright (C) 2017-2018 Dremio Corporation
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+"""
+
+import pytest
+
+from example import connect_to_dremio_flight_server_endpoint
+
+def test_basic_auth():
+    """
+    Test connection to Dremio.
+    """
+    connect_to_dremio_flight_server_endpoint("localhost",
+      "32010", "dremio", "dremio123", False, False, False)
+
+def test_simple_query():
+    """
+    Test connection to Dremio.
+    Then test a simple VALUES query.
+    """
+    query = "select * from (VALUES(1,2,3))"
+    connect_to_dremio_flight_server_endpoint("localhost",
+      "32010", "dremio", "dremio123", query, False, False)
+
+def test_bad_hostname():
+    """
+    Test connection with an incorrect server endpoint hostname.
+    """
+    pytest.xfail("Bad hostname.")
+    connect_to_dremio_flight_server_endpoint("badHostNamE",
+      "32010", "dremio", "dremio123", False, False, False)
+
+def test_bad_port():
+    """
+    Test connection with an incorrect server endpoint port.
+    """
+    pytest.xfail("Bad port.")
+    connect_to_dremio_flight_server_endpoint("localhost",
+      "12345", "dremio", "dremio123", False, False, False)
+
+def test_bad_password():
+    """
+    Test connection with an invalid password.
+    """
+    pytest.xfail("Bad port.")
+    connect_to_dremio_flight_server_endpoint("localhost",
+      "32010", "dremio", "badPassword", False, False, False)
+
+def test_non_existent_user():
+    """
+    Test connection with an invalid username.
+    """
+    pytest.xfail("Non-existent user.")
+    connect_to_dremio_flight_server_endpoint("localhost",
+      "32010", "noSuchUser", "dremio123", False, False, False)
